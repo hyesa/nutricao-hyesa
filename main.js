0b1455c2d7a63c199f748b7151060ddd80011187
Hyêsa <133382273+hyesa@users.noreply.github.com>
2024-09-20 12:13:28 -0300

    20/09/2024

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("oi Hyêsa");
+console.log("Oi Pri");
+
+var titulo= document.querySelector("h1");
+titulo.textContent = "Hyêsa Nutrição";
+
+var trPaulo = document.querySelector("#Paulo");
+
+console.log(trPaulo);
+
+var tdPeso = trPaulo.querySelector(".info-peso");
+tdPeso.textContent = "150";
