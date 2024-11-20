a9bc43f830f969b4011a9e5d0790a531366bd721
Hyêsa <133382273+hyesa@users.noreply.github.com>
2024-11-20 20:49:34 -0300

    Update form.js

--- form.js
+++ form.js
@@ -1,43 +1,38 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
+botaoAdicionar.addEventListener("click", function(event){
   event.preventDeFault();
-
+  
   var form = document.querySelector("#form-adiciona");
   
   //Extraindo os dados digitados no formulário
   
   var paciente = obtemPacienteDoFormulario(form);
-  console.log(paciente);
   
   //criando a linha e as célulasda tabela do novo paciente
+  var pacienteTr = montaTr(paciente);
 
-  var pacienteTr = document.createElement("tr");
+  var erros = validaPaciente(paciente);
   
-  var nomeTd = document.createElement("td");
-  var pesoTd = document.createElement("td");
-  var alturaTd = document.createElement("td");
-  var gorduraTd = document.createElement("td");
-  var imcTd = document.createElement("td");
-
-  nomeTd.textContent = nome;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = altura;
-  gorduraTd.textContent = gordura;
-  imcTd.textContent = calculaImc(peso,
-  altura);
-
-  pacienteTr.appendChild(nomeTd);
-  pacienteTr.appendChild(pesoTd);
-  pacienteTr.appendChild(alturaTd);
-  pacienteTr.appendChild(gorduraTd);
-  paxienteTr.apeendChild(imcTd);
+  if(erros.length > 0) {
+    exibeErros(erros);
+    return;
+  }
 
 //Aqui adicionamos a linha com todos os dados na tabela do "HTML"
 
   var tabela = document.querySelector("#tabela-pacientes");
 
   tabela.appendChild(pacienteTr);
+  form.reset();
 
+});
+
+function exibeErros(erros){
+  var ul = document.querySelector("#mensagens-erro");
+  erros.forEach(function(erro){
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
 });
 
 function obtemPacienteDoFormulario(form) {
@@ -50,4 +45,34 @@ function obtemPacienteDoFormulario(form) {
     imc: calculaImc(form.peso.value, form.altura.value)
   }
     return paciente;
+}
+
+function montaTr(paciente) {
+  var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
+    
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    paxienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+    
+    return pacienteTr;
+}
+  function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    
+    return td;
+  }
+  
+  function validaPaciente(paciente) {
+    var erros = [];
+    
+    if(!validaPeso(paciente.peso)){
+    erros.push("Peso inválido!")
+  }
+  return erros;
+  }
 }
